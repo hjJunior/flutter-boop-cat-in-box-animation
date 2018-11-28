fe9a679847651dc8bf1e76d807f6aa1fc60b05f7
Helio S. Junior <helio.junior@codeminer42.com>
2018-11-28 10:18:57 -0200

    Finishing animations

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/cat.dart';
+import 'dart:math';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -11,28 +12,63 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   Animation<double> catAnimation;
   AnimationController catController;
+  Animation<double> boxAnimation;
+  AnimationController boxController;
 
   @override
   void initState() {
     super.initState();
+    _setupAnimations();
+    boxController.forward();
+  }
 
+  void _setupAnimations() {
+    _setupBoxAnimation();
+    _setupCatAnimation();
+  }
+
+  void _setupBoxAnimation() {
+    boxController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 800)
+    );
+
+    boxAnimation = Tween(begin: pi * .6, end: pi * .65).animate(
+        CurvedAnimation(
+            curve: Curves.easeInOut,
+            parent: boxController
+        )
+    );
+
+    boxAnimation.addListener(() {
+      if (boxAnimation.status == AnimationStatus.completed) {
+        boxController.reverse();
+      } else if (boxAnimation.status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
+  }
+
+  void _setupCatAnimation() {
     catController = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 200)
+        vsync: this,
+        duration: Duration(milliseconds: 200)
     );
 
     catAnimation = Tween(begin: -35.0, end: -80.0).animate(
-      CurvedAnimation(
-        parent: catController,
-        curve: Curves.easeIn
-      )
+        CurvedAnimation(
+            parent: catController,
+            curve: Curves.easeIn
+        )
     );
   }
 
   void _startAnimation() {
     if (catController.isCompleted) {
+      boxController.forward();
       catController.reverse();
     } else if (catController.isDismissed) {
+      boxController.stop();
       catController.forward();
     }
   }
@@ -49,8 +85,10 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           child: Stack(
             overflow: Overflow.visible,
             children: <Widget>[
-              _buildBox(),
               _buildCatAnimation(),
+              _buildBox(),
+              _buildLeftFlap(),
+              _buildRightFlap(),
             ],
           ),
         )
@@ -73,5 +111,41 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
       left: 0,
     ),
     child: Cat(),
+  );
+
+  Widget _buildLeftFlap() => Positioned(
+      left: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) => Transform.rotate(
+          angle: boxAnimation.value,
+          alignment: Alignment.topLeft,
+          child: child,
+        ),
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
+      )
+  );
+
+
+
+  Widget _buildRightFlap() => Positioned(
+      right: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        builder: (context, child) => Transform.rotate(
+          angle: -boxAnimation.value,
+          alignment: Alignment.topRight,
+          child: child,
+        ),
+        child: Container(
+          height: 10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
+      )
   );
 }
